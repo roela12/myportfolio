d5d07da69824985d3a0d8e11a68d13fc74986344
Rodrigo Martinez <rmartinezjalil@gmail.com>
2025-04-14 16:56:56 -0300

    cv added

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./FooterComponent.css";
 import linkedin from "../../assets/linkedin.webp";
 import github from "../../assets/github.webp";
+import cv from "../../assets/cv.webp";
 
 const FooterComponent = () => {
   return (
@@ -11,6 +12,9 @@ const FooterComponent = () => {
       </a>
       <a href="https://github.com/roela12">
         <img class="footer__logo" src={github} alt="github logo" />
+      </a>
+      <a href="https://drive.google.com/file/d/1mI5ROf9jI3Ekk5JiNoQDdYnp2FHzMs5I/view">
+        <img class="footer__logo" src={cv} alt="cv logo" />
       </a>
     </div>
   );
