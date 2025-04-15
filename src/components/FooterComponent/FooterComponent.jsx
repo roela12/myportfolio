767f4a0b9104b8c9e531db2b673321c6279b1f53
Rodrigo Martinez <rmartinezjalil@gmail.com>
2025-04-14 21:26:58 -0300

    some animations on images

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -7,14 +7,14 @@ import cv from "../../assets/cv.webp";
 const FooterComponent = () => {
   return (
     <div className="footer">
+      <a href="https://drive.google.com/file/d/1mI5ROf9jI3Ekk5JiNoQDdYnp2FHzMs5I/view">
+        <img class="footer__logo" src={cv} alt="cv logo" />
+      </a>
       <a href="https://www.linkedin.com/in/rodrimartinezjalil/">
         <img class="footer__logo" src={linkedin} alt="linkedin logo" />
       </a>
       <a href="https://github.com/roela12">
         <img class="footer__logo" src={github} alt="github logo" />
-      </a>
-      <a href="https://drive.google.com/file/d/1mI5ROf9jI3Ekk5JiNoQDdYnp2FHzMs5I/view">
-        <img class="footer__logo" src={cv} alt="cv logo" />
       </a>
     </div>
   );
